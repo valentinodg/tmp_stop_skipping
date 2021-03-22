225091fcecee2673b0ef667d311599b7163b75b0
valentinodg <valentino.digiosaffatte@student.univaq.it>
2021-03-22 23:46:14 +0100

    generator 2

--- src/utils/generator.py
+++ src/utils/generator.py
@@ -5,37 +5,67 @@ import numpy as np
 import pandas as pd
 
 """
-     travel time disutility weight:      (float) m
+- travel time disutility weight:
+    (float) m
      
-                      bus capacity:        (int) C
+- bus capacity:
+    (int) C
                       
-travel time between adjacent stops:  (float[][]) t   [array but not dynamic, see below (!)]
+- travel time between adjacent stops:
+    (float[][]) t
+        [array but not dynamic, see below (!)]
 
-              dwell time at stop i:      (float) d   [not array, not dynamic]
+- dwell time at stop i:
+    (float) d
+        [not array, not dynamic]
               
-            travel time elasticity:      (float) e
+- travel time elasticity:
+    (float) e
             
-  total travel time from stop i to
-         stop j on a local service:  (float[][]) t  [here (!), dynamic part,
-                                                     can be simulated using a method, performance?]
+- total travel time from stop i to
+  stop j on a local service:
+    (float[][]) t
+        [here (!), dynamic part,
+        can be simulated using a method,
+        performance?]
          
-    total travel time savings from
-     running express between stops
-                           i and j:  (float[][]) c  [can be simulated using a method, performance?]
+- total travel time savings from 
+  running express between stops i and j:
+    (float[][]) c
+        [can be simulated using a method,
+        performance?]
                            
-  rate of increase in limited-stop 
-      service demand per minute of 
-travel time reduction for O-D pair
-                       k=(s^k,d^k):  (float[][]) a  
-         
+- rate of increase in limited-stop
+service demand per minute of travel
+time reduction for O-D pair k=(s^k,d^k):
+    (float[][]) a       
 """
 
 class Generator:
     def __init__(self):
+        self.S = 35
         self.m = 1.0
         self.C = 80
-#         self.t = 
         self.d = 0.5
         self.e = -0.5
-#         self.c =
-#         self.a =
+        self.t = None
+        self.c = None
+        self.a = None
+        self.__start()
+        
+    def __start(self):
+        self.t = self.__gen_t_matrix()
+        self.c = self.__gen_c_matrix()
+        self.a = self.__gen_a_matrix()
+
+    def __gen_t_matrix(self):
+        t = np.zeros((self.S, self.S))
+        return t
+    
+    def __gen_c_matrix(self):
+        c = np.zeros((self.S, self.S))
+        return c
+    
+    def __gen_a_matrix(self):
+        a = np.zeros((self.S, self.S))
+        return a
